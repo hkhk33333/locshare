a8aae71846213f1ffbf3faaab92b397f925164ee
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-09-10 18:56:28 +0300

    :arrow_up: (sdk) upgrade targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
     defaultConfig {
         applicationId = "com.test.testing"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
