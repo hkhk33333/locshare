670b0d044e56712335ae4a601982962a525dd134
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-09-10 20:38:27 +0300

    :fire:  remove unused import

--- app/src/main/java/com/test/testing/LocationService.kt
+++ app/src/main/java/com/test/testing/LocationService.kt
@@ -6,7 +6,6 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.location.Location
-import android.os.Build
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
