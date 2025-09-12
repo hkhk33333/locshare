c9868fe869555f1e76ff25687c9a7b31095f23d3
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-09-12 10:21:03 +0300

    :sparkles: (hilt) add hilt for dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,12 @@
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
     alias(libs.plugins.kover)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.detekt)
     id("kotlin-parcelize")
 }
@@ -141,6 +143,8 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/test/testing/LocShareApp.kt
+++ app/src/main/java/com/test/testing/LocShareApp.kt
@@ -1,14 +1,24 @@
 package com.test.testing
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
+@HiltAndroidApp
 class LocShareApp : Application() {
     companion object {
         private const val TAG = "LocShareApp"
     }
+
+    // TODO Delete when https://github.com/google/dagger/issues/3601 is resolved.
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
 
     override fun onCreate() {
         super.onCreate()
